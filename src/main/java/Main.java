b511d5e1c5886a7e304dc0dfc308e031c1ccf0fb
marynanesterenko <marynanesterenko.qa@gmail.com>
2025-04-13 20:29:57 -0400

    - Added Clock timing logic to measure performance - Generated an array of BigInteger values from 1 to 5000 using MakeBigIntArray - Performed sequential factorial computation with timing - Added ForkJoinPool-based execution using RecursiveFactorial for parallel computation - Printed timing and final results for both approaches

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.math.BigInteger;
+import java.time.Clock;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
@@ -17,5 +20,39 @@ public class Main {
         executor.shutdown();
         executor.awaitTermination(30, TimeUnit.SECONDS);
         System.out.println("Finished all threads");
+
+        Clock clock = Clock.systemDefaultZone();
+        long start, stop;
+        MakeBigIntArray test = new MakeBigIntArray(5000);
+
+        // Check the number of available processors
+        int nThreads = Runtime.getRuntime().availableProcessors();
+        System.out.println(nThreads);
+
+        // Printouts for threaded results
+
+
+
+        // Print outs for sequential results
+        BigInteger sum = new BigInteger("1");
+        start = clock.millis();
+        for (int i = 0; i < test.getList().length; i++) {
+            sum = sum.multiply(test.getList()[i]);
+        }
+        stop = clock.millis();
+        System.out.println("Time in ms: " + (stop - start));
+        System.out.println("Serial Result: " + sum);
+
+        RecursiveFactorial rSum = new RecursiveFactorial(test.getList());
+        ForkJoinPool pool = new ForkJoinPool(nThreads);
+
+        start = clock.millis();
+        pool.invoke(rSum);
+        stop = clock.millis();
+
+        System.out.println("Time in ms: " + (stop - start));
+
+        BigInteger result = rSum.result;
+        System.out.println("Pooled Result: " + result);
     }
 }
