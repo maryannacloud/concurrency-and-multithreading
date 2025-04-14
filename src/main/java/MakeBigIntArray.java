e676c24598e788608aaf7af0a27e1253dadd6be6
marynanesterenko <marynanesterenko.qa@gmail.com>
2025-04-13 20:30:33 -0400

    - Generates an array of BigInteger values from 1 to a given upper bound - Used to support factorial calculation in both sequential and parallel approaches

---
+++ src/main/java/MakeBigIntArray.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+
+public class MakeBigIntArray {
+
+    private final BigInteger[] list;
+
+    public MakeBigIntArray(int bounds) {
+        list = new BigInteger[bounds];
+        for (int i = 0; i < bounds; i++) {
+            list[i] = new BigInteger("" + (i + 1));
+        }
+    }
+
+    public BigInteger[] getList() {
+        return list;
+    }
+}
